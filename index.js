43ae0d20d6b03dad36401144a7a790140806e9be
Will Brennan <willbrennan314@gmail.com>
2021-11-01 19:56:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let result;
+  if (someValue <= 400) {
+    return result = 'This one is on me!';
+   } else if (someValue > 2000 && someValue < 2501) {
+    return result = 'I will gladly take your thirty bucks.';
+   } else if (someValue > 2500) {
+     return result = 'No can do.';
+   }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city){
+  return(city === 'NYC')? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous' :
+      return 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default : 
+    return 'Bye.';
+  }
+  }
